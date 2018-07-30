f43ca1ec6b1a6d02fe95cf3b8d77e3644b123b1b
Rouzrise <rouzrise@gmail.com>
2018-07-30 16:02:11 +0300

    Start React app and create Map with Google Map React

---
+++ No_react_try/foursquareTry.js
@@ -0,0 +1,24 @@
+https://api.foursquare.com/v2/venues/search?&ll=54.943433,20.15463&client_id=XLS14R0FF13HLWSQTW3OCWQIGVO22BPT2EBONMVZ54ISGVBQ&client_secret=TPHAWSJ0SEEO1DCZYIRYOJRTXVZFHOTAFIWAFGOJTFNSPRGB&v=20140806&m=foursquare
+
+https://api.foursquare.com/v2/venues/search?ll=40.7484,-73.9857&
+
+const request = require('request');
+
+request({
+  url: 'https://api.foursquare.com/v2/venues/explore',
+  method: 'GET',
+  qs: {
+    client_id: 'CLIENT_ID',
+    client_secret: 'CLIENT_SECRET',
+    ll: '40.7243,-74.0018',
+    query: 'coffee',
+    v: '20180323',
+    limit: 1
+  }
+}, function(err, res, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(body);
+  }
+});
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import GoogleMap from './Components/GoogleMap'
+
+
+class App extends React.Component {
+
+  render() {
+
+    //DESTRUCTURING
+
+    return (
+      <GoogleMap />
+    )
+  }
+}
+
+export default App
