f1105e96a5d66756e3dfd7ef8ff463d58aa9eea1
Rouzrise <rouzrise@gmail.com>
2018-07-31 17:43:40 +0300

    Add Map Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import './App.css'
-import GoogleMap from './Components/GoogleMap'
+import {GoogleApiWrapper} from 'google-maps-react';
+import Map from './Components/Map'
 
 
 class App extends React.Component {
 
   render() {
-
-    //DESTRUCTURING
-
     return (
-      <GoogleMap />
+      <Map google={this.props.google}/>
     )
   }
 }
 
-export default App
+//functionality from https://www.npmjs.com/package/google-maps-react
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyA0Y4NitHWpQhi4poD85XyNZjYDqLk1oEg'
+})(App)
